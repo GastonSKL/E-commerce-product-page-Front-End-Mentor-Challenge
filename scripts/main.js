77a5bf8b45e26b1a34f76056682f2ba8c0385b82
GastonSKL <gastonlskamlec@gmail.com>
2022-10-20 17:14:55 -0300

    EVENT LISTENERS A BOTONES
    Se le agrega funcionalidades a los botones de agregar productos al carrito, por ahora la build va estable

--- scripts/main.js
+++ scripts/main.js
@@ -38,12 +38,31 @@ console.log(botonPrincipalValor);
 
 //CARRITO Y PROFILE PICTURE //
 
+//SELECTORES//
 const carrito = document.querySelector('.header__avatar'); //CARRITO//
 const notification = document.querySelector('.header__cart--notification'); //NOTIFICACION//
 
+//FUNCION PARA CONVERTIR EL VALUE DEL IMPUT A STRING, PARA PASARSELO COMO INNERHTML A LA NOTIFICACION DEL CARRITO//
 const conversion = (value) =>{
     const string = value.toString();
     return string;
 }
+
+let resultado;
+
+//EVENT LISTENER PARA MANDAR EL VALUE DEL INPUT CUANDO SE HAGA CLICK EN EL BOTON PRINCIPAL,//
+//A LA NOTIFICACION DEL CARRITO, SE LE AGREGA UNA VALIDACION, EN EL CASO DE QUE EL VALOR SEA//
+//DISTINTO A 0, SE LE CAMBIA EL ESTILO DE DISPLAY A ESTA NOTIFICACION PARA QUE SE MUESTRE//
+//EN CASO DE QUE SEA 0, SE VUELVE A DISPLAY NONE//
+
+botonPrincipalCarrito.addEventListener('click',()=>{
+    resultado = conversion(botonPrincipalValor.value);
+    notification.innerHTML = resultado;
+    if(botonPrincipalValor.value != 0){
+        notification.style.display = 'inline-block';
+    }else if(botonPrincipalValor.value == 0){
+        notification.style.display = 'none';
+    }
+})
 
 
