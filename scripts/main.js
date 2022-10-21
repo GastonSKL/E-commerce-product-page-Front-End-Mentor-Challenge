aefeb683a21b59756c9bde46b37e4eed5cc64642
GastonSKL <gastonlskamlec@gmail.com>
2022-10-21 12:09:09 -0300

    Galeria
    Se agregan funcionalidades a galeria

--- scripts/main.js
+++ scripts/main.js
@@ -79,5 +79,111 @@ botonCompra.addEventListener('click',()=>{
     carritoModal.style.display = 'none';
 });
 
+//////////////////////////////////////////////////////////////////////////////////
+
+//Galeria principal//
+
+const imagenMain = document.querySelector('.gallery__image-container');
+const modalBackground = document.querySelector('.modal-gallery__background');
+const botonCierre = document.querySelector('.modal-gallery__close');
+const imagenSmall = document.querySelector('.gallery__thumnail');
+const imagenSmall1 = document.querySelector('.gallery__thumnail-1');
+const imagenSmall2 = document.querySelector('.gallery__thumnail-2');
+const imagenSmall3 = document.querySelector('.gallery__thumnail-3');
+const imagenSmall4 = document.querySelector('.gallery__thumnail-4');
+const imagenSmallModal1 = document.querySelector('.modal-gallery_thumnail-1');
+const imagenSmallModal2 = document.querySelector('.modal-gallery_thumnail-2');
+const imagenSmallModal3 = document.querySelector('.modal-gallery_thumnail-3');
+const imagenSmallModal4 = document.querySelector('.modal-gallery_thumnail-4');
+const contenedorImagen = document.querySelector('.modal-gallery__image-container');
+const botonSiguiente = document.querySelector('.modal-gallery__next');
+const botonAnterior = document.querySelector('.modal-gallery__previus');
+
+imagenMain.addEventListener('click',()=>{
+         modalBackground.style.display = 'block';
+});
+
+botonCierre.addEventListener('click',()=>{
+    modalBackground.style.display = 'none';
+})
+
+imagenSmall1.addEventListener('click',()=>{
+    modalBackground.style.display = 'block';
+    contenedorImagen.style.backgroundImage = 'url(images/image-product-1.jpg)'
+})
+
+imagenSmall2.addEventListener('click',()=>{
+    modalBackground.style.display = 'block';
+    contenedorImagen.style.backgroundImage = 'url(images/image-product-2.jpg)'
+})
+
+imagenSmall3.addEventListener('click',()=>{
+    modalBackground.style.display = 'block';
+    contenedorImagen.style.backgroundImage = 'url(images/image-product-3.jpg)'
+})
+
+imagenSmall4.addEventListener('click',()=>{
+    modalBackground.style.display = 'block';
+    contenedorImagen.style.backgroundImage = 'url(images/image-product-4.jpg)'
+})
+
+imagenSmallModal1.addEventListener('click',()=>{
+    contenedorImagen.style.backgroundImage = 'url(images/image-product-1.jpg)'
+})
+
+imagenSmallModal2.addEventListener('click',()=>{
+    contenedorImagen.style.backgroundImage = 'url(images/image-product-2.jpg)'
+})
+
+imagenSmallModal3.addEventListener('click',()=>{
+    contenedorImagen.style.backgroundImage = 'url(images/image-product-3.jpg)'
+})
+
+imagenSmallModal4.addEventListener('click',()=>{
+    contenedorImagen.style.backgroundImage = 'url(images/image-product-4.jpg)'
+})
+
+let contador = 1;
+const siguiente =()=>{
+    if(contador == 1){
+        contenedorImagen.style.backgroundImage = 'url(images/image-product-2.jpg)';
+        contador++;
+    }else if(contador == 2){
+        contenedorImagen.style.backgroundImage = 'url(images/image-product-3.jpg)';
+        contador++;
+    }else if(contador == 3){
+        contenedorImagen.style.backgroundImage = 'url(images/image-product-4.jpg)';
+        contador++;
+    }else if(contador == 4){
+        contenedorImagen.style.backgroundImage = 'url(images/image-product-1.jpg)';
+        contador = 1;
+    }
+    return contador;
+}
+
+const anterior = () =>{
+    if(contador == 1){
+        contenedorImagen.style.backgroundImage = 'url(images/image-product-4.jpg)';
+        contador++;
+    }else if(contador == 2){
+        contenedorImagen.style.backgroundImage = 'url(images/image-product-1.jpg)';
+        contador++;
+    }else if(contador == 3){
+        contenedorImagen.style.backgroundImage = 'url(images/image-product-2.jpg)';
+        contador++;
+    }else if(contador == 4){
+        contenedorImagen.style.backgroundImage = 'url(images/image-product-3.jpg)';
+        contador = 1;
+    }
+    return contador;
+}
+
+botonSiguiente.addEventListener('click',()=>{
+    siguiente();
+})
+
+botonAnterior.addEventListener('click',()=>{
+    anterior();
+})
 
 
